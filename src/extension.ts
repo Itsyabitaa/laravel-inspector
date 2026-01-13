2e3308db17fe65a7e5943ef9a55b220c3e89595e
kukusha <yeabsira0514@gmail.com>
2026-01-13 07:52:42 +0300

    update

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,41 @@ import * as path from 'path';
 import { parsePhp } from './analyzer/phpAst';
 import { extractMethods } from './analyzer/controllerMethods';
 import { analyzeMethod } from './analyzer/methodAnalyzer';
+import { LaravelInspectorCodeLensProvider } from './ui/codeLensProvider';
+import { refreshDiagnostics, diagnosticCollection } from './ui/diagnostics';
+import { LaravelInspectorHoverProvider } from './ui/hoverProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+	const output = vscode.window.createOutputChannel('Laravel Inspector');
+
+	context.subscriptions.push(
+		vscode.languages.registerCodeLensProvider(
+			{ language: 'php' },
+			new LaravelInspectorCodeLensProvider()
+		)
+	);
+
+	context.subscriptions.push(diagnosticCollection);
+
+	if (vscode.window.activeTextEditor) {
+		refreshDiagnostics(vscode.window.activeTextEditor.document);
+	}
+
+	context.subscriptions.push(
+		vscode.workspace.onDidSaveTextDocument(refreshDiagnostics),
+		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document)),
+		vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) refreshDiagnostics(editor.document);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.languages.registerHoverProvider(
+			{ language: 'php' },
+			new LaravelInspectorHoverProvider()
+		)
+	);
+
 	const disposable = vscode.commands.registerCommand(
 		'laravel-inspector.analyzeController',
 		() => {
@@ -29,8 +62,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 				const analyses = methods.map(m => analyzeMethod(m.node));
 
-				console.log('--- Laravel Inspector: Method Analysis ---');
-				console.table(analyses);
+				output.appendLine('--- Laravel Inspector: Method Analysis ---');
+				output.appendLine(`File: ${doc.fileName}`);
+				analyses.forEach(a => {
+					output.appendLine(`Method: ${a.methodName} | LoopDepth: ${a.maxLoopDepth} | Queries: ${a.queriesTotal} | N+1: ${a.possibleNPlusOne}`);
+				});
+				output.show(true);
 
 				const nplus = analyses.filter(a => a.possibleNPlusOne).length;
 				vscode.window.showInformationMessage(
